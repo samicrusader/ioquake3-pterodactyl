6c4f10a0f2dd6e11f3e9077fbd5838f7a36aa7a9
samicrusader <71227643+samicrusader@users.noreply.github.com>
2020-12-31 02:19:00 -0500

    Update installdemo.sh

--- installdemo.sh
+++ installdemo.sh
@@ -11,7 +11,7 @@ make
 mv build/release-linux-x86_64 ~/ioquake3
 cd ~
 mkdir -p ~/.q3a
-rm -r ~/ioq3
+rm -rf ~/ioq3
 ash -c 'wget --header="Referer: https://ioquake3.org/extras/patch-data/" "https://www.ioquake3.org/data/quake3-latest-pk3s.zip" -O- | unzip -'
 mv ~/quake3-latest-pk3s/* ~/.q3a/.
 wget 'https://konch.ie/W2rhjV0gPgAC.pk3' -O ~/.q3a/baseq3/pak0.pk3
