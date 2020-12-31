1d3dad6341e33d46f850323930239f3dcfb26ed2
samicrusader <71227643+samicrusader@users.noreply.github.com>
2020-12-31 02:24:02 -0500

    Update installdemo.sh

--- installdemo.sh
+++ installdemo.sh
@@ -4,14 +4,16 @@
 # send bug reports to whatever git repo i choose for this thing
 # i hate docker
 
-cd ~
-git clone https://github.com/ioquake/ioq3.git ~/ioq3
-cd ~/ioq3
+printf "I am currently running as $(whoami) in $(pwd)\n"
+
+cd /home/container
+git clone https://github.com/ioquake/ioq3.git /home/container/ioq3
+cd /home/container/ioq3
 make
-mv build/release-linux-x86_64 ~/ioquake3
-cd ~
-mkdir -p ~/.q3a
-rm -rf ~/ioq3
-ash -c 'wget --header="Referer: https://ioquake3.org/extras/patch-data/" "https://www.ioquake3.org/data/quake3-latest-pk3s.zip" -O- | unzip -'
-mv ~/quake3-latest-pk3s/* ~/.q3a/.
-wget 'https://konch.ie/W2rhjV0gPgAC.pk3' -O ~/.q3a/baseq3/pak0.pk3
+mv build/release-linux-x86_64 /home/container/ioquake3
+cd /home/container
+mkdir -p /home/container/.q3a
+rm -rf /home/container/ioq3
+ash -c 'wget -nv --header="Referer: https://ioquake3.org/extras/patch-data/" "https://www.ioquake3.org/data/quake3-latest-pk3s.zip" -O- | unzip -'
+mv /home/container/quake3-latest-pk3s/* /home/container/.q3a/.
+wget 'https://konch.ie/W2rhjV0gPgAC.pk3' -O /home/container/.q3a/baseq3/pak0.pk3
